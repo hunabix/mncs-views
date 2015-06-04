9fec73879635baf4fd537a20f068f87565722ff3
Alejandro Sobrevilla <hunabix@gmail.com>
2015-06-04 12:59:42 -0500

    Reacomodé las carpetas para los JS

--- footer.php
+++ footer.php
@@ -10,21 +10,12 @@
 
 </body>
 
-    <script src="js/jquery-1.10.2.js" type="text/javascript"></script>
-    <script src="js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
-    <script src="js/bootstrap.js" type="text/javascript"></script>
-    <script src="js/gsdk-checkbox.js"></script>
-    <script src="js/gsdk-radio.js"></script>
-    <script src="js/gsdk-bootstrapswitch.js"></script>
-    <script src="js/get-shit-done.js"></script>    
+    <script src="js/support/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="js/support/bootstrap.js" type="text/javascript"></script>
+    <script src="js/support/gsdk/gsdk-checkbox.js"></script>
+    <script src="js/support/gsdk/gsdk-radio.js"></script>
+    <script src="js/support/gsdk/gsdk-bootstrapswitch.js"></script>
+    <script src="js/support/gsdk/get-shit-done.js"></script>    
     <script src="js/main.js"></script>
-
-
-    <script>
-
-            
-
-    </script>
-
 
 </html>
